911fde952211bd9cf461ef2e5ab65b96d3ac8017
nikhil2325-alt <choturana2325@gmail.com>
2026-01-14 23:06:59 +0530

    no

---
+++ enjoy/dashboard/jaldi.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Etna jaldi mana gaye</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+
+<body class="d-flex justify-content-center align-items-center text-white"
+      style="background-color:#F22991; height:100vh;">
+
+  <div class="text-center">
+    <img src="../image/cute.gif" alt="Italian Trulli">
+    <h1>etna jaldi mana gaye kya btt hai </h1>
+   
+
+
+  </div>
+
+</body>
+</html>
