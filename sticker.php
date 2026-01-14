a34d293c82f15a71add0ea6667a4bdcfdd234608
nikhil2325-alt <choturana2325@gmail.com>
2026-01-14 23:11:44 +0530

    Add files via upload

---
+++ sticker.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap 5 Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+
+<body class="d-flex justify-content-center align-items-center text-white"
+      style="background-color:#F22991; height:100vh;">
+
+  <div class="text-center">
+    <img src="./image/manja.gif" alt="Italian Trulli">
+    <h1>do you love me? </h1>
+   <a href="./dashboard/jaldi.php" class="btn btn-light me-5">Yes</a>
+   <a href="./dashboard/again.php" class="btn btn-light">No</a>
+
+
+  </div>
+
+</body>
+</html>
